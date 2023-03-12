84be4dce6a0d86a1c17c641f3b1c387b61586f97
hweejae <hwejae25@gamil.com>
2023-03-12 22:38:10 +0900

    Setting. 초기설정 eslint, prettier, husky, codeOwner, PR template, directory

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb", "airbnb/hooks", "plugin:prettier/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    semi: "warn",
+    "no-unused-vars": "warn",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/self-closing-comp": "off",
+    "react/jsx-filename-extension": ["warn", { extensions: [".js"] }],
+    "no-param-reassign": 0,
+  },
+};
---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>hello world!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
