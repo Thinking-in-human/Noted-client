f49b307786948208672333c7b63cf1b63581d488
Jisook93 <leejisook0718@gmail.com>
2023-03-17 12:07:37 +0900

    Feat. Auth & 로그인 요청 구현 PR 코멘트 사항 반영(syntax 수정)

--- src/app/App.js
+++ src/app/App.js
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <>
       <GlobalStyle />
-      {loginUser.avatarImgURL === "" ? (
+      {!loginUser.avatarImgURL ? (
         <Login />
       ) : (
         <>
