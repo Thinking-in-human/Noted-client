977a1d0a1321fc2ffa5047927475ce9d4f3fcd51
hweejae <hwejae25@gamil.com>
2023-03-31 23:57:37 +0900

    Fix. 충돌 이후 에러 해결

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as pdfjs from "pdfjs-dist";
 import axios from "axios";
 
